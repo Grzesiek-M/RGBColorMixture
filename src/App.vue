3b09568db47cfe6b44483800ce8b91eeaedaada1
Grzesiek-M <grzesiek@zwarszawy.pl>
2023-04-24 14:16:35 +0200

    zad23

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <div class="container">
+
+      <!-- header -->
+      <app-header />
+
+      <!-- color mixin -->
+      <color-mixin />
+      <tab-menu :model="items"></tab-menu>
+    </div>
+  </div>
+</template>
+
+<script>
+import './styles/global.scss'
+import AppHeader from './components/AppHeader'
+import ColorMixin from './components/ColorMixin'
+import TabMenu from 'primevue/tabmenu'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    ColorMixin,
+    TabMenu
+  },
+  data () {
+    return {
+      items: [
+        {
+          label: 'Home',
+          icon: 'pi pi-fw pi-home',
+          to: '/'
+        },
+        {
+          label: 'Calendar',
+          icon: 'pi pi-fw pi-calendar',
+          to: '/calendar'
+        },
+        {
+          label: 'Edit',
+          icon: 'pi pi-fw pi-pencil',
+          to: '/edit'
+        }
+      ]
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  text-align: center;
+  padding: 2rem;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.min.css'
+
+createApp(App).mount('#app')
